677d1f017a6aa433990b69e19c8bc248546bb012
Adeeb Mohammed <adeeb2358@gmail.com>
2022-03-01 12:08:49 +0900

    Initial Commit

---
+++ WordWrap/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  WordWrap
+//
+//  Created by adeeb mohammed on 01/03/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
